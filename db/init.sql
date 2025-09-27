cf5576b0b7b3f2a779b5ffdbe7bb4ef33769ab95
olubunmi-ade <olubunmi0309@gmail.com>
2025-09-27 02:51:59 -0700

    bunmi

--- db/init.sql
+++ db/init.sql
@@ -7,4 +7,4 @@ CREATE TABLE users (
     name VARCHAR(50)
 );
 
-INSERT INTO users (name) VALUES ('Olubunmi!'), ('Anita'), ('Hakeem');
+INSERT INTO users (name) VALUES ('Bunmi!'), ('Anita'), ('Hakeem');
