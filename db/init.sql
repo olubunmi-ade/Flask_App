5942880eef543c040a498043dcf111697ab6fa1e
olubunmi-ade <olubunmi0309@gmail.com>
2025-09-27 03:06:51 -0700

    number

--- db/init.sql
+++ db/init.sql
@@ -7,4 +7,4 @@ CREATE TABLE users (
     name VARCHAR(50)
 );
 
-INSERT INTO users (name) VALUES ('Olubunmi-'), ('Anita'), ('Hakeem');
+INSERT INTO users (name) VALUES ('1. Olubunmi'), ('2. Anita'), ('3. Hakeem');
